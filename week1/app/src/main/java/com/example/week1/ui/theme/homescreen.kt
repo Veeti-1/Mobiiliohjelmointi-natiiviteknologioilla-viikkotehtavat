2e985c40c83d1314065514598ffc40bf059fce67
Veeti-1 <veeti.salomaa155@gmail.com>
2026-01-13 15:52:47 +0200

    valmis

--- week1/app/src/main/java/com/example/week1/ui/theme/homescreen.kt
+++ week1/app/src/main/java/com/example/week1/ui/theme/homescreen.kt
@@ -10,19 +10,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import com.example.week1.domain.*
+import org.intellij.lang.annotations.JdkConstants
+
 @Composable
 fun homescreen(taskslist: List<Task>){
     var tasks by remember { mutableStateOf(taskslist) }
-    Column {
-        Row(modifier = Modifier.padding(top = 22.dp)) { Button(onClick = {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Row(modifier = Modifier.padding(top = 22.dp)) {
+            Button(onClick = {
             tasks=filterByDone(tasks, done = true)
         }) {Text(text = "Show only done tasks") }
+
             Button(onClick = {
                 tasks=sortByDueDate(tasks)
             }) {Text(text = "Sort by dueDate") }
+
             Button(onClick = {
                 val nextid = tasks.maxOf { it.id } +1
                 val newTask =Task(
@@ -33,28 +41,34 @@ fun homescreen(taskslist: List<Task>){
                     dueDate = "2026-1-6",
                     done = false,
                 )
-
                 tasks = addTask(tasks, newTask)
                 println("taskit "+tasks)
             }) {Text(text="add task") }}
-        tasks.forEach { task ->
-            if(task.done){
-                Text(
-                    text = "Id: ${task.id} - ${task.title} Status: Done",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }else if(task.done == false){
-                Row {
-                    Button(onClick = {
-                    tasks = toggleDone(tasks,task.id)
-                }) {Text(text = "done") }
-                    Text(
-                        text = "Id: ${task.id} - ${task.title} Status: Task not done yet. Due Date: ${task.dueDate}",
-                        modifier = Modifier.padding(16.dp),
-                    )}
-            }
 
+            tasks.forEach { task ->
+                if (task.done) {
+
+                    Text(
+                        text = "Id: ${task.id} - ${task.title} Status: Done \n DueDate was: ${task.dueDate}",
+                        modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
+
+                    )
+                } else if (task.done == false) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Button(onClick = {
+                            tasks = toggleDone(tasks, task.id)
+                        }) { Text(text = "done") }
+                        Text(
+                            text = "Id: ${task.id} - ${task.title} \n Status: Task not done yet.\n Due Date: ${task.dueDate}",
+                            modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
+                        )
+                    }
+
+
+            }
         }
+
+
 
 
 
